2f90e25b4a83300f321a96673f5a96f3b57f9779
Guilherme <jepessilva@gmail.com>
2024-09-09 13:04:12 -0300

    Update jornadaDoHeroi.js

--- jornadaDoHeroi.js
+++ jornadaDoHeroi.js
@@ -1,16 +1,9 @@
-
-function calcularPosicaoFinal(posicaoInicial, numeroDePassos) {
-    let posicaoFinal = posicaoInicial + numeroDePassos;
-    return "Posicao final do heroi: " + posicaoFinal;
-}
-
-
-function simularJornada(posicaoInicial, numeroDePassos) {
-    let resultado = calcularPosicaoFinal(posicaoInicial, numeroDePassos);
-    console.log(resultado);
-}
-
-
-simularJornada (10, 6)
-//simularJornada(15, 3);
-//simularJornada(2, 3);
+function simularJornada(posicaoInicial, numeroDePassos){
+    let resultado = posicaoInicial + numeroDePassos;
+    return ("Posicao final do heroi: " + resultado);
+  }
+  
+  let posicaoInicial = parseInt(gets());
+  let numeroDePassos = parseInt(gets());
+  
+  console.log(simularJornada(posicaoInicial, numeroDePassos));
