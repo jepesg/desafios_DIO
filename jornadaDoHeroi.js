4f8039ca7b625e0dee69d1c198da5d781ddbeb7a
Guilherme <jepessilva@gmail.com>
2024-09-09 11:29:48 -0300

    Create jornadaDoHeroi.js

---
+++ jornadaDoHeroi.js
@@ -0,0 +1,16 @@
+
+function calcularPosicaoFinal(posicaoInicial, numeroDePassos) {
+    let posicaoFinal = posicaoInicial + numeroDePassos;
+    return "Posicao final do heroi: " + posicaoFinal;
+}
+
+
+function simularJornada(posicaoInicial, numeroDePassos) {
+    let resultado = calcularPosicaoFinal(posicaoInicial, numeroDePassos);
+    console.log(resultado);
+}
+
+
+simularJornada[(10, 6), (15, 3), (2, 3)];
+//simularJornada(15, 3);
+//simularJornada(2, 3);
