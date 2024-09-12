d5a581609febf3f9c065355f921327814334c23b
Guilherme <jepessilva@gmail.com>
2024-09-12 14:00:35 -0300

    Create combinandoNomesPokémons.js

---
+++ combinandoNomesPokémons.js
@@ -0,0 +1,10 @@
+function combinandoNomesPokemon(palavra) {
+    let palavraPokemon = palavra + "saur";
+    return (palavraPokemon)
+}
+
+var nomeEntrada = gets();
+
+var palavraGerada = combinandoNomesPokemon(nomeEntrada);
+
+console.log(palavraGerada);
